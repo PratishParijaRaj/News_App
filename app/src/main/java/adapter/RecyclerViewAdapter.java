ae480ecf9c891ae903bacfa0b2343551fc32d329
Pratish.Parija <pratish.parija@incture.com>
2018-05-21 19:24:44 +0530

    Initial commit

--- app/src/main/java/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/adapter/RecyclerViewAdapter.java
@@ -1,7 +1,10 @@
 package adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,15 +14,21 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.pratishparija.news.R;
+import com.leocardz.link.preview.library.LinkPreviewCallback;
+import com.leocardz.link.preview.library.SourceContent;
+import com.leocardz.link.preview.library.TextCrawler;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import activity.MainActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 import model.CardModel;
 
@@ -38,6 +47,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         return cardModels.get(position).getCardtype();
     }
 
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -47,6 +57,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         } else if (viewType == 2) {
             ViewGroup view = (ViewGroup) LayoutInflater.from(parent.getContext()).inflate(R.layout.card1_layout, parent, false);
             return new MyNewViewHolder(view);
+        } else if (viewType == 3) {
+            ViewGroup view = (ViewGroup) LayoutInflater.from(parent.getContext()).inflate(R.layout.preview_layout, parent, false);
+            return new MyPreviewViewHolder(view);
         } else return null;
     }
 
@@ -69,7 +82,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
             viewHolder.bodyDesc.setText(cardModels.get(position).getDescription());
             viewHolder.btnLike.setText(s3);
             viewHolder.btnComment.setText(s4);
-            Picasso.get().load(cardModels.get(position).getDescimage()).resize(400,400).into(viewHolder.bodyImageView);
+            Picasso.get().load(cardModels.get(position).getDescimage()).resize(400, 400).into(viewHolder.bodyImageView);
             Picasso.get().load(cardModels.get(position).getImageUrl()).into(viewHolder.headerImg);
         }
         if (holder instanceof MyNewViewHolder) {
@@ -86,13 +99,58 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
             viewHolder.headerHeadings.setText(cardModels.get(position).getHeading());
             viewHolder.btnLike.setText(s3);
             viewHolder.btnComment.setText(s4);
-            Picasso.get().load(cardModels.get(position).getImageUrl()).resize(400,400).centerCrop().into(viewHolder.headerImg);
-            ArrayList<String> opt =new ArrayList<>();
+            Picasso.get().load(cardModels.get(position).getImageUrl()).resize(400, 400).centerCrop().into(viewHolder.headerImg);
+            ArrayList<String> opt = new ArrayList<>();
             opt = cardModels.get(position).getOptions();
             viewHolder.radio1.setText(opt.get(0));
             viewHolder.radio2.setText(opt.get(1));
             viewHolder.radio3.setText(opt.get(2));
             viewHolder.radio4.setText(opt.get(3));
+        }
+        if (holder instanceof MyPreviewViewHolder) {
+
+            final MyPreviewViewHolder viewHolder = (MyPreviewViewHolder) holder;
+            int i2 = cardModels.get(position).getLikes();
+            int i3 = cardModels.get(position).getComments();
+            String s3 = Integer.toString(i2);
+            String s4 = Integer.toString(i3);
+            long i1 = cardModels.get(position).getTimestamp();
+            String s1 = Long.toString(i1) + "h ago";
+            viewHolder.categories.setText(cardModels.get(position).getCategories());
+            viewHolder.timestamp.setText(s1);
+            viewHolder.headerHeadings.setText(cardModels.get(position).getHeading());
+            viewHolder.btnLike.setText(s3);
+            viewHolder.btnComment.setText(s4);
+            Picasso.get().load(cardModels.get(position).getImageUrl()).resize(400, 400).centerCrop().into(viewHolder.headerImg);
+            String preview = cardModels.get(position).getUrl();
+            LinkPreviewCallback linkPreviewCallback = new LinkPreviewCallback() {
+                @Override
+
+                public void onPre() {
+                }
+
+                @Override
+
+                public void onPos(final SourceContent sourceContent, boolean b) {
+                    viewHolder.previewText.setText(sourceContent.getTitle());
+                    List<String> images = new ArrayList();
+                    images = sourceContent.getImages();
+                    Picasso.get().load(images.get(0)).into(viewHolder.preview);
+                    String str = sourceContent.getUrl();
+
+
+                }
+
+            };
+            TextCrawler textCrawler = new TextCrawler();
+            textCrawler.makePreview(linkPreviewCallback, preview);
+
+            viewHolder.preview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, "hi", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 
@@ -100,6 +158,16 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
     public int getItemCount() {
         return cardModels.size();
     }
+
+    //    @OnClick(R.id.preview)
+//    public void onViewClicked() {
+//        Toast.makeText(context, "hiiiiii", Toast.LENGTH_LONG).show();
+//        Uri uri = Uri.parse("https://android.jlelse.eu/room-store-your-data-c6d49b4d53a3");
+//        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//        context.startActivity(intent);
+//    }
+
+
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.categories)
@@ -129,6 +197,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
 
         }
     }
+
     public class MyNewViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.cardview_id_2)
         CardView cardviewId2;
@@ -154,6 +223,35 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         Button btnComment;
 
         public MyNewViewHolder(View itemView1) {
+            super(itemView1);
+            ButterKnife.bind(this, itemView1);
+
+        }
+    }
+
+    public class MyPreviewViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.cardview_id_3)
+        CardView cardviewId3;
+        @BindView(R.id.categories)
+        TextView categories;
+        @BindView(R.id.preview)
+        ImageView preview;
+        @BindView(R.id.timestamp)
+        TextView timestamp;
+        @BindView(R.id.header_img)
+        CircleImageView headerImg;
+        @BindView(R.id.header_headings)
+        TextView headerHeadings;
+        @BindView(R.id.preview_text)
+        TextView previewText;
+        @BindView(R.id.btn_like)
+        Button btnLike;
+        @BindView(R.id.btn_comment)
+        Button btnComment;
+
+
+
+        public MyPreviewViewHolder(View itemView1) {
             super(itemView1);
             ButterKnife.bind(this, itemView1);
 
--- app/src/main/java/data/SampleDataProvider.java
+++ app/src/main/java/data/SampleDataProvider.java
@@ -16,7 +16,7 @@ import adapter.RecyclerViewAdapter;
 import model.CardModel;
 
 public class SampleDataProvider {
-    private static List<CardModel>  data = new ArrayList<>();
+    private static List<CardModel> data = new ArrayList<>();
 
     public static List<CardModel> getData() {
         List<String> xl = new ArrayList<>();
@@ -24,15 +24,15 @@ public class SampleDataProvider {
         xl.add("India");
         xl.add("Australia");
         xl.add("Pakistan");
-        data.add(new CardModel(1,"Sports", (long) 12, "https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/coniglio_rabbit_small.png?alt=media&token=dda63b10-eb25-4095-9fcb-7919134a0a31", "IPL 2018 Week Highlights", "Trent Boult. 5.4 over: Boult comes around the wicket, angles one in from the corner of the crease, slight drop in pace, seaming in from just short of good length. " +
+        data.add(new CardModel(1, "Sports", (long) 12, "https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/coniglio_rabbit_small.png?alt=media&token=dda63b10-eb25-4095-9fcb-7919134a0a31", "IPL 2018 Week Highlights", "Trent Boult. 5.4 over: Boult comes around the wicket, angles one in from the corner of the crease, slight drop in pace, seaming in from just short of good length. " +
                 "Kohliat extra cover", "https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/a.png?alt=media&token=9cb72430-d90a-48e5-8f7a-e825efdcca46", 10, 11));
-        data.add(new CardModel(1,"Breaking News politics", (long) 13, "https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/coniglio_rabbit_small.png?alt=media&token=dda63b10-eb25-4095-9fcb-7919134a0a31", "Supreme Court Rejects Karnataka's Plea To Stall Finalisation", "\n" +
+        data.add(new CardModel(2, "Technology", (long) 14, "https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/a.png?alt=media&token=9cb72430-d90a-48e5-8f7a-e825efdcca46", "Who will win cricket worldcup 2019? ", 101, 110, (ArrayList<String>) xl));
+        data.add(new CardModel(3, "Food", (long) 16, "https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/a.png?alt=media&token=9cb72430-d90a-48e5-8f7a-e825efdcca46", "IPL 2018 Week Highlights", "Description", 103, 121, "https://www.nytimes.com/2018/05/08/technology/google-io.html?rref=collection%2Ftimestopic%2FAndroid&action=click&contentCollection=timestopics&region=stream&module=stream_unit&version=latest&contentPlacement=2&pgtype=collection"));
+        data.add(new CardModel(1, "Breaking News politics", (long) 13, "https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/coniglio_rabbit_small.png?alt=media&token=dda63b10-eb25-4095-9fcb-7919134a0a31", "Supreme Court Rejects Karnataka's Plea To Stall Finalisation", "\n" +
                 "Supreme Court Rejects Karnataka's Draft Cauvery Scheme\n" +
-                "The Supreme Court a issue directions \"from time to time\" on   Puducherry.","https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/b.png?alt=media&token=1fccbc9a-183a-4c7d-8ca3-e92295555148", 100, 101));
-        data.add(new CardModel(2,"Technology", (long) 14, "https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/a.png?alt=media&token=9cb72430-d90a-48e5-8f7a-e825efdcca46", "Who will win cricket worldcup 2019? ",101, 110, (ArrayList<String>) xl));
-        data.add(new CardModel(2,"Entertainment", (long) 15,"https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/a.png?alt=media&token=9cb72430-d90a-48e5-8f7a-e825efdcca46", "IPL 2018 Week Highlights", 104, 131,(ArrayList<String>) xl));
-//        data.add(new CardModel(3,"Food", (long) 16,"https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/a.png?alt=media&token=9cb72430-d90a-48e5-8f7a-e825efdcca46", "IPL 2018 Week Highlights", "Description",  103, 121,"https://www.nytimes.com/2018/05/08/technology/google-io.html?rref=collection%2Ftimestopic%2FAndroid&action=click&contentCollection=timestopics&region=stream&module=stream_unit&version=latest&contentPlacement=2&pgtype=collection"));
-//        data.add(new CardModel(3,"Politics", (long) 17,"https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/a.png?alt=media&token=9cb72430-d90a-48e5-8f7a-e825efdcca46", "IPL 2018 Week Highlights", "Description",  103, 121,"https://www.nytimes.com/2018/05/02/technology/personaltech/why-are-there-so-many-android-versions.html?rref=collection%2Ftimestopic%2FAndroid&action=click&contentCollection=timestopics&region=stream&module=stream_unit&version=latest&contentPlacement=3&pgtype=collection"));
+                "The Supreme Court a issue directions \"from time to time\" on   Puducherry.", "https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/b.png?alt=media&token=1fccbc9a-183a-4c7d-8ca3-e92295555148", 100, 101));
+        data.add(new CardModel(2, "Entertainment", (long) 15, "https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/a.png?alt=media&token=9cb72430-d90a-48e5-8f7a-e825efdcca46", "IPL 2018 Week Highlights", 104, 131, (ArrayList<String>) xl));
+        data.add(new CardModel(3, "Politics", (long) 17, "https://firebasestorage.googleapis.com/v0/b/news-e5e04.appspot.com/o/a.png?alt=media&token=9cb72430-d90a-48e5-8f7a-e825efdcca46", "IPL 2018 Week Highlights", "Description", 103, 121, "https://www.nytimes.com/2018/05/19/technology/facebook-deletion-center-germany.html?rref=collection%2Fsectioncollection%2Ftechnology&action=click&contentCollection=technology&region=rank&module=package&version=highlights&contentPlacement=1&pgtype=sectionfront"));
         return data;
     }
 }
